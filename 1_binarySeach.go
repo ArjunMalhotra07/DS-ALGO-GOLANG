36c53a71aca1202cde57169aa93f525aceee5ea3
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-08 12:49:01 +0530

    Order Agnostic Binary Search

--- 1_binarySeach.go
+++ 1_binarySeach.go
@@ -15,12 +15,17 @@ func main() {
 	}
 	fmt.Println("Your array is: ", arr)
 
-	ans := binarySearch(arr, -1)
-	fmt.Println(ans)
+	if arr[0] < arr[len(arr)-1] {
+		ans := binarySearchAsc(arr, 50)
+		fmt.Println(ans)
+	} else {
+		ans := binarySearchDesc(arr, -1)
+		fmt.Println(ans)
+	}
 
 }
 
-func binarySearch(arr []int, target int) int {
+func binarySearchAsc(arr []int, target int) int {
 	start := 0
 	end := len(arr) - 1
 	for start <= end {
@@ -31,6 +36,25 @@ func binarySearch(arr []int, target int) int {
 			end = mid - 1
 		} else if arr[mid] < target {
 			start = mid + 1
+		} else {
+			return mid
+		}
+
+	}
+	return -1
+}
+
+func binarySearchDesc(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+	for start <= end {
+		//	mid:=(start+end)/2   Might be possible k ye exceeed kr jaye int ki limit
+		mid := start + (end-start)/2
+
+		if arr[mid] > target {
+			start = mid + 1
+		} else if arr[mid] < target {
+			end = mid - 1
 		} else {
 			return mid
 		}
