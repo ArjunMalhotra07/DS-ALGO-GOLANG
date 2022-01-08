58d2aafa0500fe56abd5a3840a9451f76fa76def
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-08 12:20:07 +0530

    basic binary search

---
+++ 1_binarySeach.go
@@ -0,0 +1,40 @@
+//WE KNOE ARRAY IS SORTED IN ASC ORDER
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	fmt.Println("Enter Array Length")
+	fmt.Scanln(&n)
+	fmt.Println("Enter Array")
+	var arr = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &arr[i])
+	}
+	fmt.Println("Your array is: ", arr)
+
+	ans := binarySearch(arr, 50)
+	fmt.Println(ans)
+
+}
+
+func binarySearch(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+	for start <= end {
+		//	mid:=(start+end)/2   Might be possible k ye exceeed kr jaye int ki limit
+		mid := start + (end-start)/2
+
+		if arr[mid] > target {
+			start = mid - 1
+		} else if arr[mid] < target {
+			start = mid + 1
+		} else {
+			return mid
+		}
+
+	}
+	return -1
+}
