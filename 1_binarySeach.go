2f5188a0763773934f585ea123580cc84517bf28
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-09-21 12:01:23 +0530

    Added comments in Binary Search file for readability

--- 1_binarySeach.go
+++ 1_binarySeach.go
@@ -1,30 +1,58 @@
-//WE KNOE ARRAY IS SORTED IN ASC ORDER
+//WE KNOw ARRAY IS SORTED IN ASC ORDER
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	f := fmt.Println
+	// c := fmt.Print
+	p := fmt.Printf
 
+	// Entering length of array
+	f("Enter Array Length")
 	var n int
-	fmt.Println("Enter Array Length")
 	fmt.Scanln(&n)
-	fmt.Println("Enter Array")
+
+	// Input Array by user
+	f("Enter Array (Should be ascending or descending)")
 	var arr = make([]int, n)
 	for i := 0; i < n; i++ {
 		fmt.Scanf("%d", &arr[i])
 	}
-	fmt.Println("Your array is: ", arr)
+	f("Your array is: ", arr)
 
+	// Entering length of array
+	f("Enter Number to be searched in array")
+	var numberToBeSearched int
+	fmt.Scanln(&numberToBeSearched)
+
+	// If ascending array then different function
 	if arr[0] < arr[len(arr)-1] {
-		ans := binarySearchAsc(arr, 50)
-		fmt.Println(ans)
+		ans := binarySearchAsc(arr, numberToBeSearched)
+		if ans < 0 {
+			p("%d is not present in entered array", numberToBeSearched)
+			f()
+		} else {
+			p("%d is at index %d ", numberToBeSearched, ans)
+			f()
+		}
+
 	} else {
-		ans := binarySearchDesc(arr, -1)
-		fmt.Println(ans)
+		ans := binarySearchDesc(arr, numberToBeSearched)
+		if ans < 0 {
+			p("%d is not present in entered array", numberToBeSearched)
+			f()
+		} else {
+			p("%d is at index %d ", numberToBeSearched, ans)
+			f()
+		}
 	}
 
 }
 
+// If array is ascending
 func binarySearchAsc(arr []int, target int) int {
 	start := 0
 	end := len(arr) - 1
@@ -44,6 +72,7 @@ func binarySearchAsc(arr []int, target int) int {
 	return -1
 }
 
+// If array is descending
 func binarySearchDesc(arr []int, target int) int {
 	start := 0
 	end := len(arr) - 1
