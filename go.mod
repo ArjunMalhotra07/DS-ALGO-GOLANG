367dcc27d70ceaf2df2b7c76eef9a7078c9fc62a
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-10-26 14:36:37 +0530

    changed file structure

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/ArjunMalhotra07/DS-ALGO-GOLANG
-
-go 1.17
-
-require github.com/cheekybits/genny v1.0.0 // indirect
---
+++ graphs/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	f := fmt.Println
+	f("Hello")
+	details()
+
+}
+
+func details() {
+	f := fmt.Println
+	// p := fmt.Print
+	s := fmt.Scanln
+	var n, m int
+
+	f("Enter number of Nodes and edges")
+	s(&n, &m)
+
+	matrixDetails(m)
+
+}
+
+func matrixDetails(length int) {
+	f := fmt.Println
+	p := fmt.Print
+	s := fmt.Scanln
+
+	var mat [6][6]int
+	var e1, e2 int
+
+	adjList := [6][]int{}
+	f("Enter 6 Edges")
+
+	for i := 0; i < length; i++ {
+		s(&e1, &e2)
+		mat[e1][e2] = 1
+		mat[e2][e1] = 1
+		adjList[e1] = append(adjList[e1], e2)
+
+		adjList[e2] = append(adjList[e2], e1)
+	}
+	f()
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			p(mat[i][j])
+			p(" ")
+		}
+		f()
+	}
+	f()
+	f(adjList)
+}
+
+/*
+1 2
+1 3
+2 4
+3 4
+3 5
+4 5
+*/
